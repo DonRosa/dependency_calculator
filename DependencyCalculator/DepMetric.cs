53e6f0836812a66e9d6eb92076ffe0dc247ea4a2
DonRosa <anders75lundberg@hotmail.com>
2015-04-08 11:01:47 +0200

    Started up git in visual studio

---
+++ DependencyCalculator/DepMetric.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyCalculator {
+   public class DepMetric {
+
+        public int cn { get; set; }
+        public int cf { get; set; }
+        public int cj { get; set; }
+        public int cs { get; set; }
+        public int n { get; set; }
+
+
+        //public int CN {
+        //    set { cn = value; }
+        //}
+
+        //public void setCN(int val) {
+        //    cn = val;
+        //}
+
+        //public int CF {
+        //    set { cf = value; }
+        //}
+
+        //public int CJ {
+        //    set { cj = value; }
+        //}
+
+        //public int CS {
+        //    set { cs = value; }
+        //}
+
+        //public int N {
+        //    set { n = value; }
+        //}
+
+        public DepMetric() {
+            cn = 0;
+            cf = 0;
+            cj = 0;
+            cs = 0;
+            n = 0;
+        }
+
+        public int calcMetric() {
+            
+            int csn = calcFac(cs * n);
+            int temp1 = 1 + (5 * cf) + (4 * cj);
+            int temp2 = 1 + (5 * cn) * Convert.ToInt16(Math.Log(cn + 1, 2)) * csn;
+
+            return temp1/temp2; 
+        }
+
+        private int calcFac(int p) {
+            int result = 0;
+
+            for (int i = p; i > 1; i--) {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ DependencyCalculator/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DependencyCalculator {
+    public partial class Form1 : Form {
+
+        DepMetric depMet;
+
+
+        public Form1() {
+            InitializeComponent();
+            depMet = new DepMetric();
+            textBox1.Text = Convert.ToString(0);
+            textBox2.Text = Convert.ToString(0);
+            textBox3.Text = Convert.ToString(0);
+            textBox4.Text = Convert.ToString(0);
+            textBox6.Text = Convert.ToString(0);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+            try {
+                depMet.cn = Convert.ToInt16(textBox1.Text);
+            } catch(FormatException) {
+                depMet.cn = 0;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e) {
+            try {
+                depMet.cf = Convert.ToInt16(textBox2.Text);
+            } catch {
+                depMet.cf = 0;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e) {
+            try {
+                depMet.cj = Convert.ToInt16(textBox3.Text);
+            } catch {
+                depMet.cj = 0;
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e) {
+            try {
+                depMet.cs = Convert.ToInt16(textBox4.Text);
+            } catch {
+                depMet.cs = 0;
+            }
+        }
+
+
+        private void textBox6_TextChanged(object sender, EventArgs e) {
+            try {
+                depMet.n = Convert.ToInt16(textBox6.Text);
+            } catch {
+                depMet.n = 0;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            textBoxResult.Text = Convert.ToString(depMet.calcMetric());
+        }
+        
+      
+    }
+}
